900e4a9a6c021fb0673371cb59126b40c7058d5f
Elisabeth <elisabeth.seite@gmail.com>
2017-05-08 20:13:39 -0400

    Update Search.java

--- Search.java
+++ Search.java
@@ -24,19 +24,19 @@ class Search
 		this.archiveList = new ArrayList<Candidate>();
 	}
 
-  //add candidates to the search
+ //add candidates to the search
 	public void addCandidate(Candidate person)
 	{	
 		candidateList.add(person);
 	}
 	
-  //remove candidates from the search
+ //remove candidates from the search
 	public void removeCandidate(Candidate person)
 	{
 		candidateList.remove(person);
 	}
 	
- // Archived the candidate once it is turned down	
+ // Archived the candidate once he/she is turned down	
 	public void archiveCandidate(Candidate person)
 	{
 		candidateList.remove(person);
@@ -51,5 +51,15 @@ class Search
 		{
 			System.out.println("- " + candidateList.get(i).getName() + ", " + candidateList.get(i).getStage());
 		}
+	}
+
+  //Search analytics
+	public runAnalytics
+	{
+		int talentPool = candidateList.size() + archiveList.size();
+		int averageApplicationTime;
+		//include average calculation difference between candidate creation date and archive date
+		 
+		System.out.println("So far, you have considered " + talentPool + " candidates. On average, the candidates stayed " + "..." + "days in the system."
 	}
 }
